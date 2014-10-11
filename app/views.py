aae75b2fc662178879f737c2f7ee20fdfd5b2949
git <git@10.0.0.44>
2014-10-11 10:11:48 -0700

    check all dir

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from app import myapp
+
+@myapp.route('/')
+@myapp.route('/index')
+def index():
+    return "Hello World !"
+
+
---
+++ run.py
@@ -0,0 +1,6 @@
+#!flask/bin/python
+
+from app import myapp
+myapp.run(debug=True)
+
+
